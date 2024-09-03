9e1f60ee99e26abe44aaf6adbc94c2c06588874c
husham35 <husham35@gmail.com>
2024-09-03 23:05:21 +0000

    fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,22 +4,25 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const options = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + movieId,
+  method: 'GET'
+};
 
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+request(options, function (err, res, body) {
+  if (!err) {
     const characters = JSON.parse(body).characters;
-
-    characters.forEach((characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        } else {
-          console.error('Error fetching character:', error);
-        }
-      });
-    });
-  } else {
-    console.error('Error fetching movie:', error);
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, idx) {
+  request(characters[idx], function (err, res, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
